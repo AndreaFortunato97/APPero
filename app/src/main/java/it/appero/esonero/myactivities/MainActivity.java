8292ed1eb5774d8e04df1c9a9ab1daed6e5b9ce0
Andrea <7126728+AndreaFortunato97@users.noreply.github.com>
2019-05-16 00:44:55 +0200

    Aggiunto qualche commento e modificate/aggiunte alcune stringhe

--- app/src/main/java/it/appero/esonero/myactivities/MainActivity.java
+++ app/src/main/java/it/appero/esonero/myactivities/MainActivity.java
@@ -7,14 +7,13 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
 
 import java.util.Locale;
 
@@ -40,17 +39,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         layout = findViewById(R.id.layoutMain);
 
+        // Il seguente 'if' crea un'istanza del tasto necessario al cambio lingua
         if(!getResources().getConfiguration().locale.getLanguage().equals(Locale.ITALIAN.toString())) {
-            btnLang = new MyLangButton(this, layout, R.id.btnMainLang, R.drawable.itflag);
+            btnLang = new MyLangButton(this, layout, R.id.btnMainLang, R.drawable.itflag); // Se la lingua impostata non è 'Italiano' allora crea il tasto con la bandiera 'Italia'
         }
         else {
-            btnLang = new MyLangButton(this, layout, R.id.btnMainLang, R.drawable.enflag);
+            btnLang = new MyLangButton(this, layout, R.id.btnMainLang, R.drawable.enflag); // Se la lingua impostata è 'Italiano' allora crea il tasto con la bandiera 'Regno Unito'
         }
 
+        // Creo un'istanza 'GoogleSignInOptions', necessaria a configurare ciò che verrà chiesto nella riga (in questo caso l'email) [56]
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
 
+        // Assegno ad 'mGoogleSignInClient' l'istanza 'gso' precedentemente creata
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         btnLang.setOnClickListener(this);
@@ -60,13 +62,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onStart() {
         super.onStart();
         signInButton = findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setSize(SignInButton.SIZE_STANDARD); // Imposto la dimensione del tasto generato dalla libreria 'SignInButton' proprietaria di Google
         btnExit = findViewById(R.id.btnExit);
 
-        account = GoogleSignIn.getLastSignedInAccount(this);
+        account = GoogleSignIn.getLastSignedInAccount(this); // Assegno ad 'account' l'eventuale ultimo account Google con il quale è stato fatto l'accesso
 
-        if(account != null)
-        {
+        if(account != null) { // Se precedentemente è stato fatto l'accesso con un account Google, entra nell'if ed avvia l'activity 'LoggedInActivity'
             Intent i = new Intent(MainActivity.this, LoggedInActivity.class);
             startActivity(i);
         }
@@ -78,37 +79,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
+            case R.id.sign_in_button: // Alla pressione del tasto di Login
+                signIn(); // Esegui la funzione 'signIn()', presente alla riga [121]
                 break;
-            case R.id.btnExit:
-                finishAffinity();
+            case R.id.btnExit: // Alla pressione del tasto di uscita
+                finishAffinity(); // Chiudi tutte le activity, comprese quelle in stato di 'Pausa' e 'Stop'
                 break;
-            case R.id.btnMainLang:
-                if(btnLang.getLanguage() == R.drawable.itflag) {
-
+            case R.id.btnMainLang: // Alla pressione del tasto di cambio lingua
+                if(btnLang.getLanguage() == R.drawable.itflag) { // Se il bottone ha la bandiera 'Italia'
                     String languageToLoad  = "it";
                     Locale locale = new Locale(languageToLoad);
-                    Locale.setDefault(locale);
+                    Locale.setDefault(locale); // Imposta la lingua dell'applicazione in Italiano
                     Configuration config = new Configuration();
                     config.locale = locale;
-                    getResources().updateConfiguration(config,this.getResources().getDisplayMetrics());
+                    getResources().updateConfiguration(config,this.getResources().getDisplayMetrics()); // Aggiorna la configurazione (impostazione interna) dell'applicazione con la nuova lingua
 
+                    // Riavvio l'activity
                     Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Prima di avviare l'activity presente come secondo parametro dell'Intent (cioè MainActivity), chiudo l'attuale. In questo caso funziona come fosse un 'riavvio'
                     startActivity(intent);
                 }
-                else {
-
+                else { // Se il bottone non ha la bandiera 'Italia'
                     String languageToLoad  = "en";
                     Locale locale = new Locale(languageToLoad);
-                    Locale.setDefault(locale);
+                    Locale.setDefault(locale); // Imposta la lingua dell'applicazione in Inglrse
                     Configuration config = new Configuration();
                     config.locale = locale;
                     getResources().updateConfiguration(config,this.getResources().getDisplayMetrics());
 
+                    // Riavvio l'activity
                     Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Prima di avviare l'activity presente come secondo parametro dell'Intent (cioè MainActivity), chiudo l'attuale. In questo caso funziona come fosse un 'riavvio'
                     startActivity(intent);
                 }
 
@@ -118,29 +119,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, 1);
+        startActivityForResult(signInIntent, 1); // Avvia l'activity gestita da Google, per effettuare il Login tramite email (richiesta alla riga [53]
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) { // Entro in questa funziona quando esco dall'activity di Login creata alla riga [123]
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 1) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+        if (requestCode == 1) { // Risultato ritornato dall'avvio dell'intent 'signInIntent' (riga [123]), con requestCode pari a 1
+            try { // Il 'try' e' di sicurezza, in caso 'getLastSignInAccount dovesse fallire
 
-            try {
-                GoogleSignInAccount acc = task.getResult(ApiException.class);
-
+                // Assegna alla variabile 'account' quello con il quale è appena stato effettuato il login
                 MainActivity.account = GoogleSignIn.getLastSignedInAccount(this);
 
+                // Avvia l'activity 'LoggedInActivity'
                 Intent i = new Intent(MainActivity.this, LoggedInActivity.class);
                 startActivity(i);
 
-            } catch (ApiException e)
+            } catch (Exception e)
                 {
+                    // Se per quale motivo il 'getLastSignedInAccount' dovesse fallire, ritorna all'utente un messaggio d'errore
+                    Toast.makeText(this, getString(R.string.loginError), Toast.LENGTH_LONG).show();
                 }
         }
     }
